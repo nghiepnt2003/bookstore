665c7bfdb90ad165f78d7eb6e0912cbc0f1fbd25
nghiepnt2003 <nghiepnt2003@gmail.com>
2024-11-16 23:40:59 +0700

    fix orderDetail model

--- .history/server/src/app/models/OrderDetail_20240926150148.js
+++ .history/server/src/app/models/OrderDetail_20240926150148.js
@@ -14,7 +14,7 @@ const orderDetailSchema = new Schema(
     _id: { type: Number },
     productId: { type: Number, required: true },
     productName: { type: String, maxLength: 255, required: true },
-    productImage: { type: String },
+    productImage: { type: String, maxLength: 255 },
     productPrice: { type: Number, required: true },
     quantity: { type: Number, required: true },
   },
