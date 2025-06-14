5b07a7f62eeed035efc9dc8837de8c989a0a4372
AnhNguyet2003 <anhbnguyet@gmail.com>
2025-06-14 21:43:53 +0700

    Update AdminButton.js

--- client/src/components/AdminButton.js
+++ client/src/components/AdminButton.js
@@ -1,7 +1,6 @@
 import React, {memo} from 'react'
 
 const AdminButton = ({ children, handleOnClick, style, fw, type= 'button' }) => {
-    const a=1;
     return ( 
         <button type={type}
         className={style ? style :` my-2 px-4 py-2 rounded-md text-white bg-main text-semibold ${fw ? 'w-full' : 'w-fit'}`}
